4198ecdb1eda031be0073538e380a6cb46f378d1
aaleex0 <alex.rf.92@gmail.com>
2025-01-16 12:06:34 +0100

    hilos con servidores cliente

--- 2Eval/PruebasConexionServidor/PruebasConexionServidor/src/Ejercicio1/Servidor.java
+++ 2Eval/PruebasConexionServidor/PruebasConexionServidor/src/Ejercicio1/Servidor.java
@@ -10,16 +10,15 @@ public class Servidor {
     public static void main( String[] arg ) {
         try {
             final String comandoSalida = "";
-// Inicio la escucha del servidor en un determinado puerto
+
 
 
             ServerSocket skServidor = new ServerSocket(Puerto);
             System.out.println("Escuchando en el puerto "+Puerto);
             System.out.println("Escuchando al Ejercicio1.Cliente 1 ");
-// Aquí atiendo la petición del cliente1 ...
+
             Socket sCliente1 = skServidor.accept();
 //            System.out.println("Escuchando al Ejercicio1.Cliente 2 ");
-// Aquí atiendo la petición del cliente2 ...
 //            Socket sCliente2 = skServidor.accept();
 
             DataInputStream input = new DataInputStream(sCliente1.getInputStream());
@@ -33,8 +32,6 @@ public class Servidor {
                 output.writeUTF("Mensaje recibido del cliente 1");
             }
 
-
-// Cierro el socket
             skServidor.close();
         } catch( Exception e ) {
             System.out.println( e.getMessage() );
